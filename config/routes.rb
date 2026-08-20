a629377b80df5d4376776bf9c305bc31e4846e83
Yusuke Kawashita <kawasan.design@xs418243.xsrv.jp>
2026-08-21 02:30:57 +0900

    投稿の新規作成機能と一覧の並び順を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  
   get "/" => "home#top"
   get "about" => "home#about"
 end
+
+
 
