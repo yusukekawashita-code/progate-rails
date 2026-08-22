9d552f9327bcff9b6db9e5fd8633e8fc93f89275
Yusuke Kawashita <kawasan.design@xs418243.xsrv.jp>
2026-08-22 14:54:11 +0900

    投稿の編集・削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,35 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
     @posts = Post.all.order(created_at: :desc)
   end
   
   def show
-    @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
   end
-
+  
   def new
   end
-
+  
   def create
     @post = Post.new(content: params[:content])
     @post.save
+    redirect_to("/posts/index")
+  end
+  
+   def edit
+     @post = Post.find_by(id: params[:id])
+   end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
   
